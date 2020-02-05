2a725d78a95baf60d92cb38fc42482f8b34d69df
Eliot Henson <eliotrhenson@gmail.com>
2020-02-05 18:31:02 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,9 +7,9 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-   {:railroads => {:pieces => 4, :names => {:one => {}, :two => {}, :three => {}, :four => {}}}
+   {:railroads => {:pieces => 4, :rent_in_dollars => {}}}
 end
 
 def monopoly_with_fourth_tier
-  
+  {:railroads => {:pieces => 4, :rent_in_dollars => {:one => {}, :two => {}, :three => {}, :four => {} }}}
 end
